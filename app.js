3aa8c8ccc0ca4b9c1de8942080eaa0596b6ff40c
ii02735 <yadallee.bilaal@outlook.fr>
2020-12-13 18:50:48 +0100

    [RESPONSE] Wording for root route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const fs = require("fs")
 const path = require("path")
 
-app.get('/',(req,res) => res.json({"message": "Welcome to the API pokemon showdown ! Available resources : abilities, items, learns, moves, natures, pokemons, pokemonTier"}))
+app.get('/',(req,res) => res.json({"message": "Welcome to the data pokemon showdown API ! Available resources : abilities, items, learns, moves, natures, pokemons, pokemonTier"}))
 
 app.get("/:json_file",(req,res) => {
     
