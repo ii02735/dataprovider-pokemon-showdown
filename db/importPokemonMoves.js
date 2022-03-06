088fe18e8c0485a496d07c86ddb585f1752987ef
ii02735 <yadallee.bilaal@outlook.fr>
2022-03-06 15:28:38 +0100

    [DEPENDENCY] Bluebird integration

--- db/importPokemonMoves.js
+++ db/importPokemonMoves.js
@@ -1,43 +1,44 @@
 const { knex } = require('./db');
 const { withoutSpaces } = require('../util');
+const bluebird = require('bluebird')
 const learns = require('../learns').flatMap((learn) => learn.gen.map((gen) => ({...learn,gen})))
-let results = { table: 'pokemon_move', CREATED: 0 }
 let learnCount = 0;
 
-(async () => {
-    for(const object of learns)
-    {
 
-        learnCount++;
-        console.log(`${learnCount}/${learns.length}`)        
-        let pokemonRow = await knex('pokemon').where({name: object.pokemon, gen: object.gen}).first(['id'])
-        if(!pokemonRow){
-            pokemonRow = await knex('pokemon').where({name: withoutSpaces(object.pokemon), gen: object.gen}).first(['id'])
-            if(!pokemonRow)
-            {
-                console.log(`Pokémon ${object.pokemon} en génération ${object.gen} introuvable`)
-                continue
-            }
+bluebird.map(learns,async (object) =>
+{
+
+    learnCount++;
+    console.log(`${learnCount}/${learns.length}`)        
+    let pokemonRow = await knex('pokemon').where({name: object.pokemon, gen: object.gen}).first(['id'])
+    if(!pokemonRow){
+        pokemonRow = await knex('pokemon').where({name: withoutSpaces(object.pokemon), gen: object.gen}).first(['id'])
+        if(!pokemonRow)
+        {
+            console.log(`Pokémon ${object.pokemon} en génération ${object.gen} introuvable`)
+            return { INSERTED: 0 }
         }
-        
-        let moveRow = await knex('move').where({name: object.move, gen: object.gen}).first(['id'])
-        
-        if(!moveRow){
-            moveRow = await knex('move').where({name: withoutSpaces(object.move), gen: object.gen}).first(['id'])
-            if(!moveRow){
-                console.log(`Move ${object.move} en génération ${object.gen} introuvable`)
-                continue
-            }
-                
-        }
-
-        const samePokemonMoveRow = await knex('pokemon_move').where({pokemon_id: pokemonRow.id, move_id: moveRow.id}).first(['id'])
-        if(samePokemonMoveRow)
-            continue;
-
-        await knex('pokemon_move').insert({pokemon_id: pokemonRow.id, move_id: moveRow.id, gen: object.gen})
-        results.CREATED++;
     }
-})().then(() => console.log(results))
-    .catch((err) => console.log(err))
-    .finally(() => knex.destroy())
+    
+    let moveRow = await knex('move').where({name: object.move, gen: object.gen}).first(['id'])
+    
+    if(!moveRow){
+        moveRow = await knex('move').where({name: withoutSpaces(object.move), gen: object.gen}).first(['id'])
+        if(!moveRow){
+            console.log(`Move ${object.move} en génération ${object.gen} introuvable`)
+            return { INSERTED: 0 }
+        }
+            
+    }
+
+    const samePokemonMoveRow = await knex('pokemon_move').where({pokemon_id: pokemonRow.id, move_id: moveRow.id}).first(['id'])
+    if(samePokemonMoveRow)
+        return { INSERTED: 0 };
+
+    await knex('pokemon_move').insert({pokemon_id: pokemonRow.id, move_id: moveRow.id, gen: object.gen})
+    return { INSERTED: 1 };
+}, { concurrency: 150 } ).then((results) => {
+    console.log({ table: 'pokemon_move', INSERTED: results.reduce((sum,{ INSERTED }) => { sum += INSERTED; return sum; },0)})
+})
+.catch((err) => console.log(err))
+.finally(() => knex.destroy())
