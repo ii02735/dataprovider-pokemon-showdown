c27931b745d257a4246d315803e8289cb10c4d6b
ii02735 <yadallee.bilaal@outlook.fr>
2022-03-08 18:37:49 +0100

    [IMPORT] Tags : replace shortName by short_name

--- db/importTags.js
+++ db/importTags.js
@@ -1,7 +1,11 @@
-const { insertOrUpdate, knex, resultRecords } = require('./db');
-const fs = require('fs');
-const tags = JSON.parse(fs.readFileSync('json/tags.json'));
+const { insertOrUpdate, knex, resultRecords } = require("./db");
+const fs = require("fs");
+const tags = JSON.parse(fs.readFileSync("json/tags.json"));
 
-Promise.all(insertOrUpdate(knex, 'tag', tags))
-	.then(results => console.log(resultRecords('tag', results)))
-	.finally(() => knex.destroy());
+Promise.all(
+  insertOrUpdate(knex, "tag", tags, {
+    replaceColumns: { shortName: "short_name" },
+  })
+)
+  .then((results) => console.log(resultRecords("tag", results)))
+  .finally(() => knex.destroy());
