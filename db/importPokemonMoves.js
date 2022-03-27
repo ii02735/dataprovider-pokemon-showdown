92fabd2f1aca48eed50dd812f287a348044bb9cd
ii02735 <yadallee.bilaal@outlook.fr>
2022-03-27 19:21:55 +0200

    [DATA] Import learns : add evos and special forms (WIP)

--- db/importPokemonMoves.js
+++ db/importPokemonMoves.js
@@ -11,61 +11,150 @@ const progressBar = new cliProgress.SingleBar(
 );
 progressBar.start(learns.length, 0);
 
-bluebird
-  .map(
-    learns,
-    async (object) => {
-      progressBar.increment();
-      let pokemonRow = await knex("pokemon")
-        .where({ name: object.pokemon, gen: object.gen })
-        .first(["id"]);
-      if (!pokemonRow) {
-        pokemonRow = await knex("pokemon")
-          .where({ name: withoutSpaces(object.pokemon), gen: object.gen })
+(async () => {
+  try {
+    let results = await bluebird.map(
+      learns,
+      async (object) => {
+        progressBar.increment();
+        let pokemonRow = await knex("pokemon")
+          .where({ name: object.pokemon, gen: object.gen })
           .first(["id"]);
         if (!pokemonRow) {
-          console.log(
-            `Pokémon ${object.pokemon} en génération ${object.gen} introuvable`
-          );
-          return { INSERTED: 0 };
+          pokemonRow = await knex("pokemon")
+            .where({
+              usage_name: withoutSpaces(object.pokemon),
+              gen: object.gen,
+            })
+            .first(["id"]);
+          if (!pokemonRow) {
+            console.log(
+              `Pokémon ${object.pokemon} en génération ${object.gen} introuvable`
+            );
+            return { INSERTED: 0 };
+          }
         }
-      }
 
-      let moveRow = await knex("move")
-        .where({ name: object.move, gen: object.gen })
-        .first(["id"]);
-
-      if (!moveRow) {
-        moveRow = await knex("move")
-          .where({ name: withoutSpaces(object.move), gen: object.gen })
+        let moveRow = await knex("move")
+          .where({ name: object.move, gen: object.gen })
           .first(["id"]);
+
         if (!moveRow) {
-          console.log(
-            `Move ${object.move} en génération ${object.gen} introuvable`
-          );
-          return { INSERTED: 0 };
+          moveRow = await knex("move")
+            .where({ usage_name: withoutSpaces(object.move), gen: object.gen })
+            .first(["id"]);
+          if (!moveRow) {
+            console.log(
+              `Move ${object.move} en génération ${object.gen} introuvable`
+            );
+            return { INSERTED: 0 };
+          }
         }
-      }
 
-      const samePokemonMoveRow = await knex("pokemon_move")
-        .where({ pokemon_id: pokemonRow.id, move_id: moveRow.id })
-        .first(["id"]);
-      if (samePokemonMoveRow) return { INSERTED: 0 };
+        const samePokemonMoveRow = await knex("pokemon_move")
+          .where({ pokemon_id: pokemonRow.id, move_id: moveRow.id })
+          .first(["id"]);
+        if (samePokemonMoveRow) return { INSERTED: 0 };
 
-      await knex("pokemon_move").insert({
-        pokemon_id: pokemonRow.id,
-        move_id: moveRow.id,
-        gen: object.gen,
-      });
-      return { INSERTED: 1 };
-    },
-    { concurrency: 150 }
-  )
-  .then((results) => {
+        await knex("pokemon_move").insert({
+          pokemon_id: pokemonRow.id,
+          move_id: moveRow.id,
+          gen: object.gen,
+        });
+        return { INSERTED: 1 };
+      },
+      { concurrency: 150 }
+    );
+
     console.log({
       table: "pokemon_move",
       INSERTED: results.reduce((sum, { INSERTED }) => sum + INSERTED, 0),
     });
-  })
-  .catch((err) => console.log(err))
-  .finally(() => knex.destroy());
+
+    let forms = await knex("pokemon")
+      .select("id", "base_form_id")
+      .whereNotNull("base_form_id");
+    progressBar.start(forms.length, 0);
+    console.log("Importing learns for special forms...");
+    results = await bluebird.map(
+      forms,
+      async (pokemon) => {
+        progressBar.increment();
+        let learns = await knex("pokemon_move").where({
+          pokemon_id: pokemon.base_form_id,
+        });
+        let result = { INSERTED: 0 };
+        for (const learn of learns) {
+          let existantLearn = await knex("pokemon_move")
+            .where({
+              pokemon_id: pokemon.id,
+              move_id: learn.move_id,
+              gen: learn.gen,
+            })
+            .first(["id"]);
+          if (existantLearn) continue;
+          await knex("pokemon_move").insert({
+            pokemon_id: pokemon.id,
+            move_id: learn.move_id,
+            way: learn.way,
+            gen: learn.gen,
+          });
+          result.INSERTED++;
+        }
+        return result;
+      },
+      { concurrency: 150 }
+    );
+
+    console.log({
+      table: "pokemon_move (base forms)",
+      INSERTED: results.reduce((sum, { INSERTED }) => sum + INSERTED, 0),
+    });
+
+    forms = await knex("pokemon")
+      .select("id", "pre_evo_id")
+      .whereNotNull("pre_evo_id");
+    console.log("Importing learns for pre evolutions...");
+    console.log();
+    progressBar.start(forms.length, 0);
+
+    results = await bluebird.map(
+      forms,
+      async (pokemon) => {
+        progressBar.increment();
+        let learns = await knex("pokemon_move").where({
+          pokemon_id: pokemon.pre_evo_id,
+        });
+        let result = { INSERTED: 0 };
+        for (const learn of learns) {
+          let existantLearn = await knex("pokemon_move")
+            .where({
+              pokemon_id: pokemon.id,
+              move_id: learn.move_id,
+              gen: learn.gen,
+            })
+            .first(["id"]);
+          if (existantLearn) continue;
+          await knex("pokemon_move").insert({
+            pokemon_id: pokemon.id,
+            move_id: learn.move_id,
+            way: learn.way,
+            gen: learn.gen,
+          });
+          result.INSERTED++;
+        }
+        return result;
+      },
+      { concurrency: 150 }
+    );
+
+    console.log({
+      table: "pokemon_move (pre evos)",
+      INSERTED: results.reduce((sum, { INSERTED }) => sum + INSERTED, 0),
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    knex.destroy();
+  }
+})();
