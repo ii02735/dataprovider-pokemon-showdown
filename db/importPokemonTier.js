4f2dc6a4df8dd2877c99ec1d03d8b74b9275b192
ii02735 <yadallee.bilaal@outlook.fr>
2022-03-28 10:09:02 +0200

    [IMPORT] Pokemon tiers : catch not found tier error

--- db/importPokemonTier.js
+++ db/importPokemonTier.js
@@ -3,31 +3,51 @@ const pokemonTiers = require("../pokemonTier");
 const { withoutSpaces } = require("../util");
 const results = { table: "pokemon", UPDATED: 0 };
 
-Promise.all(
-  pokemonTiers.map(
-    async ({ pokemon: name, tier: short_name = null, gen, technically }) => {
-      let usage_name = null;
-      let rowPokemon = await knex("pokemon").where({ name, gen }).first(["id"]);
-      if (!rowPokemon) {
-        usage_name = name;
-        rowPokemon = await knex("pokemon")
-          .where({ usage_name, gen })
-          .first(["id"]);
-        if (!rowPokemon) {
-          console.log(`Pokémon ${name} introuvable en génération ${gen}`);
-          return;
+(async () => {
+  try {
+    await Promise.all(
+      pokemonTiers.map(
+        async ({
+          pokemon: name,
+          tier: short_name = null,
+          gen,
+          technically,
+        }) => {
+          let usage_name = null;
+          let rowPokemon = await knex("pokemon")
+            .where({ name, gen })
+            .first(["id"]);
+          if (!rowPokemon) {
+            usage_name = name;
+            rowPokemon = await knex("pokemon")
+              .where({ usage_name, gen })
+              .first(["id"]);
+            if (!rowPokemon) {
+              console.log(`Pokémon ${name} introuvable en génération ${gen}`);
+              return;
+            }
+          }
+          let rowTier = short_name
+            ? await knex("tier").where({ short_name, gen }).first(["id"])
+            : await knex("tier").where({ name: "Untiered", gen }).first(["id"]);
+
+          if (!rowTier) {
+            throw new Error(
+              `Pokemon "${name}" in gen ${gen} : Tier ${short_name} cannot be found`
+            );
+          }
+
+          await knex("pokemon")
+            .update({ tier_id: rowTier.id, technically })
+            .where({ [usage_name ? "usage_name" : "name"]: name, gen });
+          results.UPDATED++;
         }
-      }
-      let rowTier = short_name
-        ? await knex("tier").where({ short_name, gen }).first(["id"])
-        : await knex("tier").where({ name: "Untiered", gen }).first(["id"]);
-      await knex("pokemon")
-        .update({ tier_id: rowTier.id, technically })
-        .where({ [usage_name ? "usage_name" : "name"]: name, gen });
-      results.UPDATED++;
-    }
-  )
-)
-  .then(() => console.log(results))
-  .catch((err) => console.log(err))
-  .finally(() => knex.destroy());
+      )
+    );
+    console.log(results);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    knex.destroy();
+  }
+})();
