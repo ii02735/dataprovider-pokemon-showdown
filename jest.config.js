0469735476cf9be750a287d9daaa16b73c4b19fe
ii02735 <yadallee.bilaal@outlook.fr>
2022-03-30 21:16:12 +0200

    [DEPENDENCY] Installation of Jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  modulePathIgnorePatterns: [
+    "<rootDir>/pokemon-showdown",
+    "<rootDir>/Smogon-Stats-Viewer/pokemon-showdown",
+  ],
+  testPathIgnorePatterns: ["/node_modules/"],
+  coveragePathIgnorePatterns: ["/node_modules/"],
+  testMatch: ["<rootDir>/tests/**/*.test.js"],
+};
