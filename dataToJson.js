3d22b9138f2fa37d836d26062b40babed8135162
ii02735 <yadallee.bilaal@outlook.fr>
2022-04-06 00:11:08 +0200

    [FILE] External JS files moved to provider dir

--- dataToJson.js
+++ dataToJson.js
@@ -1,16 +1,17 @@
 // TODO retirer la fin des pokemons missingno + CAP + pokemon studio
 "use strict";
 
-const { writeFile } = require("./util");
-
-const abilities = require("./abilities");
-const items = require("./items");
-const learns = require("./learns");
-const moves = require("./moves");
-const natures = require("./natures");
-const pokemons = require("./pokemon");
-const pokemonTier = require("./pokemonTier");
-const types = require("./types");
+const path = require("path");
+const { loadResource, LIBS, PROVIDER } = require("./libs/fileLoader");
+const { writeFile } = loadResource(LIBS, "util");
+const abilities = loadResource(PROVIDER, "abilities");
+const items = loadResource(PROVIDER, "items");
+const learns = loadResource(PROVIDER, "learns");
+const moves = loadResource(PROVIDER, "moves");
+const natures = loadResource(PROVIDER, "natures");
+const pokemons = loadResource(PROVIDER, "pokemon");
+const pokemonTier = loadResource(PROVIDER, "pokemonTier");
+const types = loadResource(PROVIDER, "types");
 
 writeFile("abilities", abilities);
 
