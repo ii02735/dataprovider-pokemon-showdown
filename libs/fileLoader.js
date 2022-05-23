fc4eef51a83685fce1ba572a8858ff88ad257604
ii02735 <yadallee.bilaal@outlook.fr>
2022-05-23 23:48:58 +0200

    [LIB] FileLoader : add pokemon showdown simulator

--- libs/fileLoader.js
+++ libs/fileLoader.js
@@ -1,6 +1,7 @@
 const path = require("path");
 module.exports.PROVIDER = "PROVIDER";
 module.exports.POKEMON_SHOWDOWN_RESOURCE = "POKEMON_SHOWDOWN_RESOURCE";
+module.exports.POKEMON_SHOWDOWN_SIMULATOR = "POKEMON_SHOWDOWN_SIMULATOR";
 module.exports.LIBS = "LIBS";
 module.exports.JSON = "JSON";
 
@@ -16,6 +17,14 @@ module.exports.loadResource = (resourceType, ...pathArray) => {
         "..",
         "pokemon-showdown",
         ".data-dist",
+        ...pathArray
+      ));
+    case this.POKEMON_SHOWDOWN_SIMULATOR:
+      return require(path.join(
+        __dirname,
+        "..",
+        "pokemon-showdown",
+        ".sim-dist",
         ...pathArray
       ));
     case this.JSON:
