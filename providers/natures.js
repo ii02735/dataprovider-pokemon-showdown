f0a909329dd74ca659fe6d93f84fc1ad4f9a8f5f
ii02735 <yadallee.bilaal@outlook.fr>
2022-05-27 23:01:19 +0200

    [FIX][DATA] Natures : fix collection iteration

--- providers/natures.js
+++ providers/natures.js
@@ -1,12 +1,10 @@
 const { Dex } = require("pokemon-showdown");
 
-const natures = Object.entries(Dex.natures.all()).map(
-  ({ id: key, name, plus, minus }) => {
-    const nature = { name, usageName: key };
-    if (plus) nature[plus] = 1;
-    if (minus) nature[minus] = -1;
-    return nature;
-  }
-);
+const natures = Dex.natures.all().map(({ id: key, name, plus, minus }) => {
+  const nature = { name, usageName: key };
+  if (plus) nature[plus] = 1;
+  if (minus) nature[minus] = -1;
+  return nature;
+});
 
 module.exports = natures;
