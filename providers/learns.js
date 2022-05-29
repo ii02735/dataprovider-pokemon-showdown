c40666be15af377241a469bad305562e363af48f
ii02735 <yadallee.bilaal@outlook.fr>
2022-05-29 23:47:46 +0200

    [DATA][LEARNS] Fix moves inheritance from Smogon's Dex

--- providers/learns.js
+++ providers/learns.js
@@ -1,6 +1,7 @@
 const { loadResource, LIBS } = require("../libs/fileLoader");
 const { LAST_GEN, pokemonIsStandard } = loadResource(LIBS, "util");
 const { Dex } = require("pokemon-showdown");
+const { Species } = require("pokemon-showdown/.sim-dist/dex-species");
 let learns = [];
 
 /**
@@ -9,7 +10,7 @@ let learns = [];
  * cannot be learnt for a specific gen. So we must clean it first.
  * @param {*} pokemonLearnset
  * @param {int} gen desired gen
- * @returns an array with the name of the moves of the correct gen
+ * @returns an object with the name of the moves of the correct gen
  */
 const getEligibleMovesForGen = (pokemonLearnset, gen) => {
   return Object.entries(pokemonLearnset)
@@ -19,9 +20,9 @@ const getEligibleMovesForGen = (pokemonLearnset, gen) => {
       ).sort();
       return genArray[0] <= gen;
     })
-    .flatMap(([moveKey, _]) => moveKey);
+    .flatMap(([moveKey, _]) => moveKey)
+    .reduce((acc, move) => ({ ...acc, [move]: move }), {});
 };
-
 /**
  * Fetch the correct learnset's asset
  * @param {int} gen the desired gen
@@ -45,61 +46,46 @@ let typesForHiddenPower = Dex.types
   .filter((type) => type.name !== "Normal" && type.name !== "Fairy")
   .map((type) => type.name);
 
+/**
+ *
+ * @param {Species} species
+ * @param {int} gen
+ */
+const genLearnsetForSpecies = (species, gen) => {
+  let result = getEligibleMovesForGen(DexLearnset(gen, species) || {}, gen);
+  let otherLearnset = {};
+  while (species.prevo) {
+    species = Dex.mod(`gen${gen}`).species.get(species.prevo);
+    otherLearnset = DexLearnset(gen, species) || {};
+    result = { ...result, ...getEligibleMovesForGen(otherLearnset, gen) };
+  }
+  console.log(result);
+  return result;
+};
+
 for (let gen = 1; gen <= LAST_GEN; gen++) {
   const pokemonsFromShowdown = Dex.mod(`gen${gen}`)
     .species.all()
     .filter((pokemon) => pokemonIsStandard(pokemon));
   for (const pokemonFromShowdown of pokemonsFromShowdown) {
-    let pokemonLearns = DexLearnset(gen, pokemonFromShowdown);
-    let otherLearnset = null;
-    if (pokemonLearns)
-      pokemonLearns = getEligibleMovesForGen(pokemonLearns, gen);
-    if (pokemonFromShowdown.prevo) {
-      let prevoFromShowdown = Dex.mod(`gen${gen}`).species.get(
-        pokemonFromShowdown.prevo
-      );
-      otherLearnset = DexLearnset(gen, prevoFromShowdown);
-      if (otherLearnset) {
-        pokemonLearns = {
-          ...pokemonLearns,
-          ...getEligibleMovesForGen(otherLearnset, gen),
-        };
-        pokemonLearns = Object.values(pokemonLearns);
-      }
-    }
+    pokemonLearns = genLearnsetForSpecies(pokemonFromShowdown, gen);
+
     if (pokemonFromShowdown.baseSpecies !== pokemonFromShowdown.name) {
       let baseSpeciesFromShowdown = Dex.mod(`gen${gen}`).species.get(
         pokemonFromShowdown.baseSpecies
       );
-      otherLearnset = DexLearnset(gen, baseSpeciesFromShowdown);
-      if (pokemonIsStandard(baseSpeciesFromShowdown) && otherLearnset) {
+      if (pokemonIsStandard(baseSpeciesFromShowdown)) {
         pokemonLearns = {
           ...pokemonLearns,
-          ...getEligibleMovesForGen(otherLearnset, gen),
-        };
-        pokemonLearns = Object.values(pokemonLearns);
-      }
-    }
-    if (pokemonFromShowdown.otherFormes) {
-      let learnsFromOtherForms = {};
-      for (const otherForm of pokemonFromShowdown.otherFormes) {
-        let otherFormFromShowdown = Dex.mod(`gen${gen}`).species.get(otherForm);
-        if (!pokemonIsStandard(otherFormFromShowdown)) continue;
-        otherLearnset = DexLearnset(gen, otherFormFromShowdown);
-        if (!otherLearnset) continue;
-        learnsFromOtherForms = {
-          ...learnsFromOtherForms,
-          ...getEligibleMovesForGen(otherLearnset, gen),
+          ...genLearnsetForSpecies(baseSpeciesFromShowdown, gen),
         };
       }
-      pokemonLearns = { ...pokemonLearns, ...learnsFromOtherForms };
-      pokemonLearns = Object.values(pokemonLearns);
     }
     if (pokemonLearns) {
       if (gen >= 2) {
         const generatedLearns = makeLearnsObject(
           pokemonFromShowdown,
-          pokemonLearns,
+          Object.values(pokemonLearns),
           gen
         );
         generatedLearns.moves = generatedLearns.moves.concat(
@@ -107,7 +93,13 @@ for (let gen = 1; gen <= LAST_GEN; gen++) {
         );
         learns.push(generatedLearns);
       } else
-        learns.push(makeLearnsObject(pokemonFromShowdown, pokemonLearns, gen));
+        learns.push(
+          makeLearnsObject(
+            pokemonFromShowdown,
+            Object.values(pokemonLearns),
+            gen
+          )
+        );
     }
   }
 }
