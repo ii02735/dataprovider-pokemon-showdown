8ab7f311dc0836a0d4b5b7b1c7b831781f5ee766
ii02735 <yadallee.bilaal@outlook.fr>
2022-09-10 20:17:52 +0200

    [SCRIPT] Don't load dataToJson deps for remote

--- bin/dataToJson.js
+++ bin/dataToJson.js
@@ -1,23 +1,32 @@
-import { ModdedDex } from "../src/pokemon-showdown/.sim-dist/dex.js";
-import AbilitiesProvider from "../src/providers/abilities.js";
-import PokemonProvider from "../src/providers/pokemon.js";
-import ItemProvider from "../src/providers/items.js";
-import TypeProvider from "../src/providers/types.js";
-import MovesProvider from "../src/providers/moves.js";
-import LearnProvider from "../src/providers/learns.js";
-import NaturesProvider from "../src/providers/natures.js";
-import PokemonTierProvider from "../src/providers/pokemonTier.js";
-import { writeFile } from "../src/libs/util.js";
+let dataToJsonFunction = () => { console.error("The dataToJson mustn't be run in production") }
 
-export default function () {
-  console.log("Generating JSON from providers data");
-  const dex = new ModdedDex();
-  writeFile("abilities.json", new AbilitiesProvider(dex));
-  writeFile("pokemons.json", new PokemonProvider(dex));
-  writeFile("items.json", new ItemProvider(dex));
-  writeFile("types.json", new TypeProvider(dex));
-  writeFile("moves.json", new MovesProvider(dex));
-  writeFile("learns.json", new LearnProvider(dex));
-  writeFile("natures.json", new NaturesProvider(dex));
-  writeFile("pokemonTier.json", new PokemonTierProvider(dex));
+if(!process.env.NODE_ENV || process.env.NODE_ENV != "prod"){
+
+  const { ModdedDex } = require("../src/pokemon-showdown/.sim-dist/dex.js");
+  const AbilitiesProvider = require("../src/providers/abilities.js");
+  const PokemonProvider = require("../src/providers/pokemon.js");
+  const ItemProvider = require("../src/providers/items.js");
+  const TypeProvider = require("../src/providers/types.js");
+  const MovesProvider = require("../src/providers/moves.js");
+  const LearnProvider = require("../src/providers/learns.js");
+  const NaturesProvider = require("../src/providers/natures.js");
+  const PokemonTierProvider = require("../src/providers/pokemonTier.js");
+  const { writeFile } = require("../src/libs/util.js");
+
+  dataToJsonFunction = () => {
+    console.log("Generating JSON from providers data");
+    const dex = new ModdedDex();
+    writeFile("abilities.json", new AbilitiesProvider(dex));
+    writeFile("pokemons.json", new PokemonProvider(dex));
+    writeFile("items.json", new ItemProvider(dex));
+    writeFile("types.json", new TypeProvider(dex));
+    writeFile("moves.json", new MovesProvider(dex));
+    writeFile("learns.json", new LearnProvider(dex));
+    writeFile("natures.json", new NaturesProvider(dex));
+    writeFile("pokemonTier.json", new PokemonTierProvider(dex));
+  }
+
 }
+
+export default dataToJsonFunction
+
