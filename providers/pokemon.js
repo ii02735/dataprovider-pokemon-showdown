a3323587e656853e6dc8dcfe101e67e463a45a32
GeoDaz <gfrydz2@gmail.com>
2022-11-11 16:31:00 +0100

    Add gen9

--- providers/pokemon.js
+++ providers/pokemon.js
@@ -68,7 +68,7 @@ for (let gen = 1; gen <= LAST_GEN; gen++) {
     .filter(
       (pokemon) =>
         isStandard(pokemon, gen, pokemon.num > 0) &&
-        !(pokemon.forme && /.*Totem/.test(pokemon.forme) && gen === 8)
+        !(pokemon.forme && /.*Totem/.test(pokemon.forme) && gen != 7)
     );
   for (const pokemonFromShowdown of pokemonsFromShowdown) {
     pokemonsCollection.push(makePokemonObject(pokemonFromShowdown, gen));
