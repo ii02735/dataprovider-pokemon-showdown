b2fda266fa94a1766a3360eca0f5301b68ac4d9f
ii02735 <yadallee.bilaal@outlook.fr>
2024-02-04 00:46:35 +0100

    Output CSV for differences in pokemonTier import

--- db/importPokemonTier.js
+++ db/importPokemonTier.js
@@ -1,5 +1,7 @@
 const { loadResource, JSON, LIBS } = require("../libs/fileLoader");
 const { knex } = require("./db");
+const csv = require("fast-csv");
+const fs = require("fs");
 const pokemonTiers = loadResource(JSON, "pokemonTier.json");
 const { removeParenthesis } = loadResource(LIBS, "util");
 const results = { table: "pokemon", UPDATED: 0 };
@@ -9,68 +11,116 @@ const catchError = (err, resolve) => {
   resolve();
 };
 
-Promise.all(
-  pokemonTiers.map(
-    ({ pokemon: name, tier: short_name, gen, technically }, i) => {
-      return new Promise((resolve, reject) => {
-        knex("pokemon")
-          .where({ name, gen })
-          .first(["id", "usage_name"])
-          .then((rowPokemon) => {
-            if (!rowPokemon) {
-              console.warn(
-                `Pokemon "${name}" in gen ${gen} cannot be found : skipping...`
-              );
-              resolve();
-              return;
-            }
+/**
+ * Object that'll store all tiers.
+ * Structure : <shortName>-<gen>: id
+ */
+const tiers = {};
 
-            short_name = removeParenthesis(short_name);
-            if (short_name === "Illegal" || short_name === "Unreleased") {
-              short_name = null;
-            }
-            knex("tier")
-              .where(
-                short_name ? { short_name, gen } : { name: "Untiered", gen }
-              )
-              .first(["id", "name"])
-              .then((rowTier) => {
-                if (!rowTier) {
-                  catchError(
-                    new Error(
-                      `Pokemon "${name}" in gen ${gen} : Tier ${short_name} cannot be found`
-                    ),
-                    resolve
-                  );
-                }
+const pokemonsStore = [];
 
+knex("pokemon")
+  .join("tier", "tier.id", "=", "pokemon.tier_id")
+  .select(["pokemon.name as pokemon", "tier.name as tier", "pokemon.gen"])
+  .then((pokemons) => {
+    for (const { pokemon, tier, gen } of pokemons) {
+      pokemonsStore.push({ pokemon, tier, newTier: null, gen });
+    }
+  })
+  .then(() => {
+    knex("tier")
+      .select(["id", "short_name", "gen"])
+      .then((rows) => {
+        for (const { id, shortName, gen } of rows) {
+          tiers[`${shortName}-${gen}`] = id;
+        }
+        return tiers;
+      })
+      .then((tiers) => {
+        Promise.all(
+          pokemonTiers.map(
+            ({ pokemon: name, tier: short_name, gen, technically }, i) => {
+              return new Promise((resolve, reject) => {
                 knex("pokemon")
-                  .update({ tierId: rowTier.id, technically })
-                  .where({ id: rowPokemon.id, gen })
-                  .then(() => {
-                    results.UPDATED++;
-                    resolve();
+                  .where({ name, gen })
+                  .first(["id", "usage_name"])
+                  .then((rowPokemon) => {
+                    if (!rowPokemon) {
+                      console.warn(
+                        `For Pokemon "${name}" in gen ${gen} cannot be found : skipping...`
+                      );
+                      resolve();
+                      return;
+                    }
+                    let tierId = null;
+                    short_name = removeParenthesis(short_name);
+                    if (
+                      short_name === "Illegal" ||
+                      short_name === "Unreleased"
+                    ) {
+                      tierId = tiers[`Untiered-${gen}`];
+                    } else if (!tiers[`${short_name}-${gen}`]) {
+                      console.warn(
+                        `For Pokemon ${name} : Tier ${short_name} cannot be found in ${gen}, setting to Untiered`
+                      );
+                      tierId = tiers[`Untiered-${gen}`];
+                    } else {
+                      tierId = tiers[`${short_name}-${gen}`];
+                    }
+
+                    knex("pokemon")
+                      .update({ tierId, technically })
+                      .where({ id: rowPokemon.id, gen })
+                      .then(() => {
+                        results.UPDATED++;
+                        resolve();
+                      })
+                      .catch((e) =>
+                        catchError(
+                          new Error(
+                            `Tier update has failed for ${name}, ${e.message}`
+                          ),
+                          resolve
+                        )
+                      );
                   })
                   .catch((e) => catchError(e, resolve));
-              })
-              .catch((e) =>
-                catchError(
-                  new Error(`Tier update has failed for ${name}, ${e.message}`),
-                  resolve
-                )
-              );
+              });
+            }
+          )
+        )
+          .then(() => {
+            const stream = csv.format({
+              headers: ["pokemon", "tier", "newTier", "gen"],
+              delimiter: ";",
+            });
+            stream.pipe(fs.createWriteStream("pokemon_tier_result.csv"));
+            knex("pokemon")
+              .join("tier", "tier.id", "=", "pokemon.tier_id")
+              .select([
+                "pokemon.name as pokemon",
+                "tier.name as newTier",
+                "pokemon.gen",
+              ])
+              .then((pokemons) => {
+                for (let i = 0; i < pokemons.length; i++) {
+                  pokemonsStore[i]["newTier"] =
+                    pokemonsStore[i]["tier"] === pokemons[i]["newTier"]
+                      ? null
+                      : pokemons[i]["newTier"];
+                  if (pokemonsStore[i]["newTier"]) {
+                    stream.write(pokemonsStore[i]);
+                  }
+                }
+                stream.end();
+                knex.destroy();
+              });
           })
-          .catch((e) => catchError(e, resolve));
+          .catch((err) => {
+            console.error(err);
+          })
+          .finally(() => {
+            knex.destroy();
+          });
       });
-    }
-  )
-)
-  .then(() => {
-    console.log(results);
-  })
-  .catch((err) => {
-    console.error(err);
-  })
-  .finally(() => {
-    knex.destroy();
   });
