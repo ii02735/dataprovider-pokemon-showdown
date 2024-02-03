ccf1355d28a3459ce2f8fdb79b411e3961e42583
GeoDaz <gfrydz2@gmail.com>
2024-02-03 11:19:31 +0100

    Fix pokemon tier import

--- db/importPokemonTier.js
+++ db/importPokemonTier.js
@@ -4,62 +4,71 @@ const pokemonTiers = loadResource(JSON, "pokemonTier.json");
 const { removeParenthesis } = loadResource(LIBS, "util");
 const results = { table: "pokemon", UPDATED: 0 };
 
-(async () => {
-  try {
-    await Promise.all(
-      pokemonTiers.map(
-        async ({
-          pokemon: name,
-          tier: short_name = null,
-          gen,
-          technically,
-        }) => {
-          let usage_name = null;
-          let rowPokemon = await knex("pokemon")
-            .where({ name, gen })
-            .first(["id"]);
-          if (!rowPokemon) {
-            usage_name = name;
-            rowPokemon = await knex("pokemon")
-              .where({ usage_name, gen })
-              .first(["id"]);
+const cathError = (err, resolve) => {
+  console.error(err);
+  resolve();
+};
+
+Promise.all(
+  pokemonTiers.map(
+    ({ pokemon: name, tier: short_name, gen, technically }, i) => {
+      return new Promise((resolve, reject) => {
+        knex("pokemon")
+          .where(function () {
+            // lambda function to use "this" in knex
+            this.where({ name }).orWhere({ usage_name: name });
+          })
+          .andWhere({ gen })
+          .first(["id", "usage_name"])
+          .then((rowPokemon) => {
             if (!rowPokemon) {
-              console.log(`Pokémon ${name} introuvable en génération ${gen}`);
-              return;
+              cathError(
+                new Error(`Pokemon "${name}" in gen ${gen} cannot be found`),
+                resolve
+              );
             }
-          }
-          let rowTier =
-            short_name && short_name != "Illegal" && short_name != "Unreleased"
-              ? await knex("tier")
-                  .where({
-                    short_name: removeParenthesis(short_name),
-                    gen,
+
+            short_name = removeParenthesis(short_name);
+            if (short_name === "Illegal" || short_name === "Unreleased") {
+              short_name = null;
+            }
+            knex("tier")
+              .where(
+                short_name ? { short_name, gen } : { name: "Untiered", gen }
+              )
+              .first(["id", "name"])
+              .then((rowTier) => {
+                if (!rowTier) {
+                  cathError(
+                    Error(
+                      `Pokemon "${name}" in gen ${gen} : Tier ${short_name} cannot be found`
+                    ),
+                    resolve
+                  );
+                }
+
+                knex("pokemon")
+                  .update({ tierId: rowTier.id, technically })
+                  .where({ id: rowPokemon.id, gen })
+                  .then(() => {
+                    results.UPDATED++;
+                    resolve();
                   })
-                  .first(["id"])
-              : await knex("tier")
-                  .where({ name: "Untiered", gen })
-                  .first(["id"]);
-
-          if (!rowTier) {
-            throw new Error(
-              `Pokemon "${name}" in gen ${gen} : Tier ${short_name} cannot be found`
-            );
-          }
-
-          await knex("pokemon")
-            .update({ tierId: rowTier.id, technically })
-            .where({
-              [usage_name ? "usage_name" : "name"]: name,
-              gen,
-            });
-          results.UPDATED++;
-        }
-      )
-    );
+                  .catch((e) => cathError(e, resolve));
+              })
+              .catch((e) => cathError(e, resolve));
+          })
+          .catch((e) => cathError(e, resolve));
+      });
+    }
+  )
+)
+  .then(() => {
     console.log(results);
-  } catch (err) {
-    console.log(err);
-  } finally {
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+  .finally(() => {
     knex.destroy();
-  }
-})();
+  });
